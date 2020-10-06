4b7cb2d8ef1579ad7a868d87e3a9823cca546d3a
Felix Ackuayi <felix.ackuayi@amalitech.org>
2020-10-06 21:47:40 +0000

    New JS function added to file as practiced

--- myapp.js
+++ myapp.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+/*console.log("Hello World");
 
 console.log(200);
 console.log(true);
@@ -6,4 +6,48 @@ console.log(null);
 
 var num = "Kofi Nimo"
 
-console.log("string",10.10,true,false,null, num);
+console.log("string",10.10,true,false,null, num);*/
+
+var car = {
+
+    color : "Red",
+    speed : 200,
+    drive : function (){return "drive"}
+
+};
+
+//Array Literal
+var shoppinglist =[
+
+
+]
+var sugar = "grannules";
+//Embedded Object
+var autoRide = {
+
+      make : "volve",
+      speed: 160,
+      engine: {
+          size: 2.0,
+          make: "BMW",
+          fuel: "petrol",
+          pistons:[{ maker: "BMW"}, {maker: sugar}]
+      },
+      drive: function (){return "drive"}//this is called method in a Car object
+};
+
+//Variable name is a memory Pointer.This is an embeded array
+var array = [
+    "string",
+    100,
+    ["embeded", 200],
+    {car: "ford"}, //object in array
+    function (){
+    return "drive";
+    }
+]
+
+
+//Accessing member of the autoRide Object
+console.log(autoRide.make);
+console.log(autoRide.drive());
